d3b94d8380cd084f4573ea3cda82eb119a33a682
sandy <sandyks.lo27@gmail.com>
2021-08-31 15:38:46 -0700

    commit tree 65ff67a0dfc160f18cab62e8a22e5940816c26ae

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-let divide;
+let num1 = 100
+let num2 = 2000
 
+let divide = function() {
+    return num2 / num1;
+}
+divide();
+
+let square = (x) => x * x;
+square();
+
+
+let a = 3;
+let b = 4;
+
+let add = (a,b) => a + b
